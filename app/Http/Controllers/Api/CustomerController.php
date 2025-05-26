c7f59a5d81c6b269f41d52f34d5bf0154eba2edf
Zer-lowex <154529753+Zer-lowex@users.noreply.github.com>
2025-05-26 09:58:25 +0800

    Removed undeclared symbol

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -7,7 +7,6 @@ use App\Models\Category;
 use App\Models\Product;
 use App\Models\ReservedProducts;
 use App\Models\Sale;
-use App\Models\StockMovement;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
