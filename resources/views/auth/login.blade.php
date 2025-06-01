373f07aaa427ac255d6820417d60eb3ab9a1d0ad
Zer-lowex <154529753+Zer-lowex@users.noreply.github.com>
2025-06-02 03:36:13 +0800

    New Title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,7 +21,7 @@
             <div class="promo-content">
                 <img src="{{ asset('assets/images/kc.png') }}" alt="Logo" class="promo-image">
                 <h2>KC PRIME SUITE</h2>
-                <p>POS AND ENTERPRISE MANAGEMENT SYSTEM</p>
+                <p>POS WITH RESERVATION MANAGEMENT SYSTEM</p>
             </div>
         </div>
 
